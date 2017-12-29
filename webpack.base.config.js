c880a05c205a8d97983cf56f05adb83e97e4ff47
Vladimir-Romanets <vovasik83@mail.ru>
2017-12-29 14:02:41 +0200

    Change config of webpack

---
+++ webpack.base.config.js
@@ -0,0 +1,42 @@
+import Config from 'webpack-config';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public/js/')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                exclude: [/node_modules/],
+                loader: "babel-loader",
+                query: {
+                    presets: ['env', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            filename: '../index.html',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin('../css/styles.css')
+    ]
+};
